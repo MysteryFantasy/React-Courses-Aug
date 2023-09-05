8e0b7d5d633c9d8431120286c2adb36b2da33d45
MysteryFantasy <mystery.fantasy132@gmail.com>
2023-09-05 03:51:50 +0300

    Added filter to the CRUD App.

--- practice_lesson_05/src/services/toDoService.js
+++ practice_lesson_05/src/services/toDoService.js
@@ -1,10 +1,7 @@
-//-- посилання на API
 const API = `https://61498bf2035b3600175ba32f.mockapi.io/exampleTodo`;
 
-//-- виклик API методом GET
 const getToDoListFromApi = () => fetch(API).then(data => data.json());
 
-// -- функція для зміни об'єкта або його властивості в масиві в БД
 const changeToDoItemInApi = (id, item) => fetch(API+`/${id}`,
 {
     method: `PUT`,
@@ -14,13 +11,11 @@ const changeToDoItemInApi = (id, item) => fetch(API+`/${id}`,
     body: JSON.stringify(item),
 }).then((data) => data.json());
 
-// -- функція для видалення об'єкта з масиву в БД
 const deletedToDoItemInApi = (id) => fetch(API+`/${id}`,
 {
     method: `DELETE`
 }).then((data) => data.json()); 
 
-//-- функція для додавання нового елементу в масив БД
 const addToDoItemToApi = (item) => fetch(API,
 {
     method: `POST`,
